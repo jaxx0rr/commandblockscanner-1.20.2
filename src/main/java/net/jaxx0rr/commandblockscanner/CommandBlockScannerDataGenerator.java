17fbf70b154a5320e9740172f00178ea4fb1ebf9
jaxx0rr <jaxx0rr@gmail.com>
2024-07-13 23:05:38 +0300

    v1

--- src/main/java/net/jaxx0rr/commandblockscanner/CommandBlockScannerDataGenerator.java
+++ src/main/java/net/jaxx0rr/commandblockscanner/CommandBlockScannerDataGenerator.java
@@ -6,6 +6,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class CommandBlockScannerDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+	//2
 	}
 }
